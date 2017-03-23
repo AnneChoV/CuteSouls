882d9fa35d908797194bc773a6b802c08d298090
Amber <amberleejulian@gmail.com>
2017-03-23 14:26:21 +1300

    Imported 2D etc

---
+++ Cute Souls/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public float health;
+    public float movementSpeed;
+    public Vector2 facingDirection;
+    public Vector2 attackingDirection;
+    
+	void Start () {
+        facingDirection = new Vector2(1, 0);
+        attackingDirection = new Vector2(1, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InitializeStats(float newhealth, float newMovementSpeed)
+    {
+        health = newhealth;
+        movementSpeed = newMovementSpeed;
+    }
+}
