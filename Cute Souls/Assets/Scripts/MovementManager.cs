0082c32b2bb488c75983c897e1fb7e2c804c7780
Amber <amberleejulian@gmail.com>
2017-03-27 01:48:43 +1300

    Most of Character Contoller

--- Cute Souls/Assets/Scripts/MovementManager.cs
+++ Cute Souls/Assets/Scripts/MovementManager.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class MovementManager : MonoBehaviour {  //This is a class shared by player and enemies to handle movements.
 
     //    //Should make own force for left and right cause the unity rigidbody one is shit for this.
-    public Vector2 facingDirection;
-    public Vector2 attackingDirection;
+    public Vector2 m_facingDirection;
+    public Vector2 m_attackingDirection;
+
+    public float m_MoveSpeed;
+    public float m_maxSpeed;
 
     [ReadOnly] public Rigidbody2D m_rigidBody;
     [ReadOnly] public CharacterStats m_Stats;
@@ -24,22 +27,54 @@ public class MovementManager : MonoBehaviour {  //This is a class shared by play
 
     public void HandleMoveLeft()
     {
-        m_rigidBody.AddForce(new Vector2(-m_Stats.movementSpeed, 0.0f), ForceMode2D.Force);
-        Mathf.Clamp(m_rigidBody.velocity.x, -10.0f, 10.0f); //Keep it between those values
-        if (m_rigidBody.velocity.x < -10.0f)
+        float mod = 1.0f; //mod = air control
+                          //Friction.
+
+        float efficacy;
+        if (m_rigidBody.velocity.x > 0)
         {
-            m_rigidBody.velocity = new Vector2(-10.0f, m_rigidBody.velocity.y);
+            efficacy = 1.0f;
         }
+        else
+        {
+            efficacy = 1.0f - Mathf.Clamp(Mathf.Abs(m_rigidBody.velocity.x) / m_maxSpeed, 0.01f, 1.0f);
+        }
+
+        //* air control
+        m_rigidBody.AddForce(new Vector2(m_MoveSpeed * -1 * efficacy * mod, 0.0f) * Time.deltaTime / Time.timeScale, ForceMode2D.Force);
+
+
+        ////If grounded
+        //float efficacy = Mathf.Abs(m_rigidBody.velocity.x) - m_maxSpeed;
+        //if (efficacy > 0.0f)
+        //{
+        //    m_rigidBody.AddForce(new Vector2(-m_rigidBody.velocity.x * efficacy, 0.0f) * Time.deltaTime / Time.timeScale, ForceMode2D.Force);
+        //}
+        //m_rigidBody.AddForce(new Vector2(-m_Stats.movementSpeed, 0.0f), ForceMode2D.Force);
+        //Mathf.Clamp(m_rigidBody.velocity.x, -10.0f, 10.0f); //Keep it between those values
+        //if (m_rigidBody.velocity.x < -10.0f)
+        //{
+        //    m_rigidBody.velocity = new Vector2(-10.0f, m_rigidBody.velocity.y);
+        //}
     }
 
     public void HandleMoveRight()
     {
-        m_rigidBody.AddForce(new Vector2(m_Stats.movementSpeed, 0.0f), ForceMode2D.Force);
-        Mathf.Clamp(m_rigidBody.velocity.x, -10.0f, 10.0f); //Keep it between those values
-        if (m_rigidBody.velocity.x > 10.0f)
+        float mod = 1.0f; //mod = air control
+        //Friction.
+        float efficacy;
+        if (m_rigidBody.velocity.x < 0)
         {
-            m_rigidBody.velocity = new Vector2(10.0f, m_rigidBody.velocity.y);
+            efficacy = 1.0f;
         }
+        else
+        {
+            efficacy = 1.0f - Mathf.Clamp(Mathf.Abs(m_rigidBody.velocity.x) / m_maxSpeed, 0.01f, 1.0f);
+        }
+
+
+        //* air control
+        m_rigidBody.AddForce(new Vector2(m_MoveSpeed * efficacy * mod, 0.0f) * Time.deltaTime / Time.timeScale, ForceMode2D.Force);
     }
 
 
