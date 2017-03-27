a9ceb19b25ae0ffb4bb899562c0811f368422a31
Anne <miss.anne.c@hotmail.com>
2017-03-27 16:32:19 +1300

    Raycasting and projectiles

--- Cute Souls/Assets/Scripts/Player.cs
+++ Cute Souls/Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+
     public CharacterStats Stats;
 
 
@@ -10,9 +11,40 @@ public class Player : MonoBehaviour {
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+
+
+        // Raycasting all objects with enemy tag, left and right
+        RaycastHit2D[] leftHit = Physics2D.RaycastAll(transform.position, -transform.right, 5.0f);
+        RaycastHit2D[] rightHit = Physics2D.RaycastAll(transform.position, transform.right, 5.0f);
+
+        for (int i = 0; i < leftHit.Length; i++)
+        {
+            if (leftHit[i].collider.tag == "Enemy")
+            {
+                RaycastHit2D hit = leftHit[i];
+
+                Debug.Log("There is an enemy to my left!");
+                Debug.Log(hit.collider.name);
+            }
+
+           
+        }
+
+        for (int i = 0; i < rightHit.Length; i++)
+        {
+            if (rightHit[i].collider.tag == "Enemy")
+            {
+                RaycastHit2D hit = rightHit[i];
+
+                Debug.Log("There is an enemy to my right!");
+                Debug.Log(hit.collider.name);
+            }
+        }
+ 
+
+
+    }
 }
