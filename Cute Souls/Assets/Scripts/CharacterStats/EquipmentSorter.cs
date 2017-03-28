e2bf94c64b6bb8573ba66f53840558b60eb3b358
Amber <amberleejulian@gmail.com>
2017-03-29 01:09:43 +1300

    Started on equipment manager
    Spent half an hour before I got made to sleep. :<

---
+++ Cute Souls/Assets/Scripts/CharacterStats/EquipmentSorter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentSorter : MonoBehaviour {
+
+    //[ReadOnly] public CSVFileReader m_csvFileReader;
+    public TextAsset equipmentFile;
+    string[,] equipment;
+
+
+    private void OnValidate()
+    {
+       // m_csvFileReader = GetComponent<CSVFileReader>();
+        equipment = CSVFileReader.ReadData(equipmentFile);
+    }
+
+    void Start()
+    {
+        SortEquipment();
+    }
+
+    public void SortEquipment()
+    {
+        int numberOfClasses = equipment.GetLength(0);
+        //Probably reference each of the classes via an archetype array and input their available equipments via their reference. Probably wanna make the character ones static?
+    }
+    
+}
