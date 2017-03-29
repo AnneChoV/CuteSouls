571b78b7225efb37e77e31c2af24f891f75bd345
Anne <miss.anne.c@hotmail.com>
2017-03-29 16:26:50 +1300

    Enemy AI into one script
    Must. Expand.

---
+++ Cute Souls/Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    [Header("Player")]
+    [ReadOnly] public Player player;
+    public float playerRange;
+
+    [Header("Enemy")]
+    public Transform launchPoint;
+    public GameObject enemyProjectile;
+    public float moveSpeed;
+    [ReadOnly] public bool moveRight;
+
+
+    [Header("Enemy Wallcheck")]
+    public Transform wallCheck;
+    public Transform edgeCheck;
+    public LayerMask Wall;
+    public float wallCheckRadius;
+
+    private bool hittingWall;
+    private bool notAtEdge;
+
+    [Header("Projectile")]
+    public float waitInbetween; // Delay between each projectile
+    private float shotCounter; // Counter for the delay
+
+
+    private void OnValidate()
+    {
+       player = FindObjectOfType<Player>();
+       shotCounter = waitInbetween;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        PatrolBehaviour();
+        ShootAtPlayer();
+	}
+
+    void OnSeePlayer()
+    {
+
+    }
+
+    void DistanceFromPlayer()
+    {
+
+    }
+
+    void PatrolBehaviour()
+    {
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, Wall);
+
+        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, Wall);
+
+        if (hittingWall || !notAtEdge)
+        {
+            moveRight = !moveRight;
+        }
+
+        if (moveRight)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+
+    void ChaseBehaviour()
+    {
+
+    }
+
+    void ShootAtPlayer()
+    {
+        // Create debug line to see range (Only in Scene Mode)
+        Debug.DrawLine(new Vector3(transform.position.x - playerRange, transform.position.y, transform.position.z), new Vector3(transform.position.x + playerRange, transform.position.y, transform.position.z));
+
+        // Tick tock
+        shotCounter -= Time.deltaTime;
+
+        if (transform.localScale.x < 0 && player.transform.position.x > transform.position.x
+            && player.transform.position.x < transform.position.x + playerRange && shotCounter < 0)
+        {
+            Instantiate(enemyProjectile, launchPoint.position, launchPoint.rotation);
+            shotCounter = waitInbetween;
+        }
+
+        if (transform.localScale.x > 0 && player.transform.position.x < transform.position.x
+            && player.transform.position.x > transform.position.x - playerRange && shotCounter < 0)
+        {
+            Instantiate(enemyProjectile, launchPoint.position, launchPoint.rotation);
+            shotCounter = waitInbetween;
+        }
+    }
+
+    void MeleeHitPlayer()
+    {
+
+    }
+
+
+}
---
+++ Cute Souls/Assets/Scripts/UI/TestHealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestHealthBar : MonoBehaviour {
+
+    public int playerHealth;
+    public GameObject playerEnergySlider;
+    public GameObject playerEnergySliderRT;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        // Player HP bar stuff here
+
+	}
+}
